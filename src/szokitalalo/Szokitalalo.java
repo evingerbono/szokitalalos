6ece99ad3b16d581fec43e9f0814826e97d59d64
Mate Dekany <96132472+dekanymate@users.noreply.github.com>
2022-03-23 14:40:53 +0100

    alap

---
+++ src/szokitalalo/Szokitalalo.java
@@ -0,0 +1,17 @@
+
+package szokitalalo;
+
+import java.util.Scanner;
+
+
+public class Szokitalalo {
+
+
+    public static void main(String[] args) {
+        String[] szavak = {"kf","lt","ap"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérek egy szót: ");
+        String szo = sc.nextLine();
+    }
+    
+}
